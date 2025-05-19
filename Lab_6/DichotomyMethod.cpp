50a032cfb95ba7b3198f8a8512aea51f5539cddf
user <user@WIN-SSPQPS065IM>
2025-05-19 19:22:39 +0300

    Updated .gitignore to exclude build files

---
+++ Lab_6/DichotomyMethod.cpp
@@ -0,0 +1,50 @@
+﻿#include "DichotomyMethod.h"
+
+// Конструктор
+DichotomyMethod::DichotomyMethod(void) : a(0.0), b(0.0), eps(0.0) {
+}
+
+// Деструктор
+DichotomyMethod::~DichotomyMethod(void) {
+
+}
+
+double DichotomyMethod::f(double x) {
+    return x - 2 + sin(1 / x);  // Приклад: f(x) = x - 2 + sin(1/x)
+}
+
+
+// Метод для знаходження кореня методом дихотомії
+int DichotomyMethod::count(double& x) {
+    int iterations = 0;  // Лічильник ітерацій
+    auto f = [](double x) { return x - 2 + sin(1 / x); };  // Лямбда-вираз для f(x)
+
+    while ((b - a) > eps) {
+        double c = (a + b) / 2;
+        if (f(c) == 0) {
+            x = c;
+            break;
+        }
+        if (f(a) * f(c) < 0) {
+            b = c;
+        }
+        else {
+            a = c;
+        }
+        x = c;
+        iterations++;
+    }
+    return iterations;  // Повертаємо кількість ітерацій
+}
+
+
+// Метод для зміни обмежень
+void DichotomyMethod::setVolumes(double vol_a, double vol_b) {
+    a = vol_a;
+    b = vol_b;
+}
+
+// Метод для зміни точності
+void DichotomyMethod::setTolerance(double vol_eps) {
+    eps = vol_eps;
+}
---
+++ Lab_6/DichotomyMethod.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+
+#ifndef DYHOTOMIA_CLASS_H
+#define DYHOTOMIA_CLASS_H
+
+class DichotomyMethod {
+private:
+    double a, b, eps;
+
+public:
+    // Конструктор
+    DichotomyMethod(void);
+    ~DichotomyMethod(void);
+
+    // Функція для пошуку кореня
+    double f(double x);
+
+    // Метод для знаходження кореня
+    int count(double& x);
+
+    // Методи для зміни параметрів
+    void setVolumes(double vol_a, double vol_b);
+    void setTolerance(double vol_eps);
+};
+
+#endif
